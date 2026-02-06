68ec8f44a473427e225fa65bb46c7bb032b4a798
foyfox58 <foy9323@gmail.com>
2026-02-06 15:49:48 +0700

    feat: implement authentication with JWT and add user registration/login APIs

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
+import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
 import { client } from "./utils/db.js";
+import authRouter from "./apps/auth.js";
 
 async function init() {
   const app = express();
-  const port = 4000;
+  const port = 4001;
+  dotenv.config();
 
   await client.connect();
 
   app.use(cors());
   app.use(bodyParser.json());
   app.use("/posts", postRouter);
+  app.use("/auth", authRouter);
 
   app.get("/", (req, res) => {
     res.send("Hello World!");

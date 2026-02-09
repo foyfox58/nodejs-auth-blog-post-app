9ee10362967711baca2ad3633c2f35aee8781921
foyfox58 <foy9323@gmail.com>
2026-02-09 15:31:38 +0700

    feat: integrate JWT interceptor and enhance authentication flow with logout functionality

--- server/apps/posts.js
+++ server/apps/posts.js
@@ -1,11 +1,36 @@
 import { ObjectId } from "mongodb";
 import { Router } from "express";
 import { db } from "../utils/db.js";
+import { protect } from "../middlewares/protect.js";
 
 const postRouter = Router();
+postRouter.use(protect);
 
 // 🐨 Todo: Exercise #5
 // นำ Middleware `protect` มาใช้กับ `postRouter` ด้วย Function `app.use`
+import jwt from "jsonwebtoken";
+
+export const protect = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Token has invalid format",
+    });
+  }
+
+  const tokenWithoutBearer = token.split(" ")[1];
+
+  jwt.verify(tokenWithoutBearer, process.env.SECRET_KEY, (err, payload) => {
+    if (err) {
+      return res.status(401).json({
+        message: "Token is invalid",
+      });
+    }
+    req.user = payload;
+    next();
+  });
+};
 
 postRouter.get("/", async (req, res) => {
   const status = req.query.status;
